b05cab39a9c6629d586246de4455c03100856108
Sergiu <sergiucodes@gmail.com>
2022-09-02 20:23:58 +0300

    ADDED DIALOG DOB DATEPICKER - WORKING - UPDATED UI FEATURES AND ADDED ICONS

--- app/src/main/java/com/example/petkeeper/ui/AddPetDialog.kt
+++ app/src/main/java/com/example/petkeeper/ui/AddPetDialog.kt
@@ -1,8 +1,8 @@
 package com.example.petkeeper.ui
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
@@ -11,6 +11,8 @@ import com.example.petkeeper.R
 import com.example.petkeeper.data.database.room.entity.Pet
 import com.example.petkeeper.databinding.DialogAddPetBinding
 import com.example.petkeeper.tools.AddDialogListener
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPetDialog(context: Context, var addDialogListener: AddDialogListener) :
     AppCompatDialog(context) {
@@ -19,6 +21,8 @@ class AddPetDialog(context: Context, var addDialogListener: AddDialogListener) :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val calendar = Calendar.getInstance()
 
         binding = DataBindingUtil.inflate(
             LayoutInflater.from(context),
@@ -29,19 +33,43 @@ class AddPetDialog(context: Context, var addDialogListener: AddDialogListener) :
         setContentView(binding.root)
 
         binding.btnSave.setOnClickListener {
+
             val petName = binding.etPetName.text.toString()
             val petSpecies = binding.etPetSpecies.text.toString()
-            val petDateOfBirth = binding.etPetDob.text.toString()
+            val petDateOfBirth = binding.tvDob.text.toString()
 
             if (petName.isEmpty() || petSpecies.isEmpty() || petDateOfBirth.isEmpty()) {
                 Toast.makeText(context, "All fields are required!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
             val pet = Pet(petName, petSpecies, petDateOfBirth)
             addDialogListener.onAddButtonClicked(pet)
             dismiss()
-            Log.d("AddDialog","Clicked save " + pet.toString())
         }
+
+        binding.btnDatepicker.setOnClickListener {
+
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    calendar.set(Calendar.YEAR, year)
+                    calendar.set(Calendar.MONTH, monthOfYear)
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                    val myFormat = "dd.MM.yyyy"
+                    val sdf = SimpleDateFormat(myFormat, Locale.UK)
+                    binding.tvDob.text = sdf.format(calendar.time)
+                }
+
+            DatePickerDialog(
+                context,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         binding.btnCancel.setOnClickListener {
             cancel()
         }
--- app/src/main/java/com/example/petkeeper/ui/PetListFragment.kt
+++ app/src/main/java/com/example/petkeeper/ui/PetListFragment.kt
@@ -1,5 +1,7 @@
 package com.example.petkeeper.ui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
